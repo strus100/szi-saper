d5cc2c22c4cb02ba9e36742f0f03e44c38cdffde
Kuba Kowalczyk <kubabrag12>
2018-04-23 17:39:48 +0200

    wall cannot be last field on the map

--- grid.py
+++ grid.py
@@ -16,10 +16,17 @@ class Grid:
         for y in range(0, height):
             horizontal = []
             for x in range(0, width):
-                is_wall_field = random.randrange(12) != 1
+                is_wall_field = self.get_is_wall_field(width, x, y)
                 field = Field(x, y, {'is_bomb': False}, field_width, field_height, is_wall_field, self.is_bomb_field(is_wall_field))
                 horizontal.insert(len(horizontal), field)
             self.grid.insert(len(self.grid), horizontal)
+
+    def get_is_wall_field(self, width, x, y):
+        if (x == (width) and (y == (width))):
+            is_wall_field = False
+        else:
+            is_wall_field = random.randrange(12) != 1
+        return is_wall_field
 
     def is_bomb_field(self, is_walkable):
         if is_walkable:
