a73ba283efc68193676f28237ac606efcfa8ddca
Krzysztof Jozefowicz <krzysztof.jozefowicz@appunite.com>
2018-06-04 23:02:15 +0200

    add spec to id3

--- id3/lib/id3.ex
+++ id3/lib/id3.ex
@@ -3,27 +3,6 @@ defmodule ID3 do
   @derive [Poison.Encoder]
   # is_beeping: yes/no | metal_detector_beeping: no/weak/hard | is_dugged_up: true/false | war_here?: no/march_of_troops/trenches
   @data_indexes %{is_beeping: 0, metal_detector_beeping: 1, is_dugged_up: 2, war_here: 3, decision: 4}
-  # @data_indexes %{outlook: 0, temp: 1, humidity: 2, wind: 3, decision: 4}
-
-  # def data do
-  #   # outlook / temp / humidity / wind / decision
-  #   [
-  #     ["Sunny", "Hot", "High", "Weak", 0],
-  #     ["Sunny", "Hot", "High", "Strong", 0],
-  #     ["Overcast", "Hot", "High", "Weak", 1],
-  #     ["Rain", "Mild", "High", "Weak", 1],
-  #     ["Rain", "Cool", "Normal", "Weak", 1],
-  #     ["Rain", "Cool", "Normal", "Strong", 0],
-  #     ["Overcast", "Cool", "Normal", "Strong", 1],
-  #     ["Sunny", "Mild", "High", "Weak", 0],
-  #     ["Sunny", "Cool", "Normal", "Weak", 1],
-  #     ["Rain", "Mild", "Normal", "Weak", 1],
-  #     ["Sunny", "Mild", "Normal", "Strong", 1],
-  #     ["Overcast", "Mild", "High", "Strong", 1],
-  #     ["Overcast", "Hot", "Normal", "Weak", 1],
-  #     ["Rain", "Mild", "High", "Strong", 0]
-  #   ]
-  # end
 
   def data do
     [
@@ -41,7 +20,7 @@ defmodule ID3 do
     ]
   end
 
-  def generate_tree(tree, data) when tree == %{} do
+  def generate_tree(data) do
     case is_yes_or_no(data) do
       :yes ->
         "YES"
@@ -52,17 +31,21 @@ defmodule ID3 do
           @data_indexes
           |> Enum.filter(fn {x, _y} -> x != :decision end)
           |> Enum.map(fn {x, _y} -> Atom.to_string(x) end)
-          |> get_highest_gain(data, {"", 0})
+          |> get_factor_with_highest_gain(data, {"", 0})
 
-        tree = Map.put(tree, "value", factor)
+        tree = Map.put(%{}, "value", factor)
+
         child_nodes_values = get_factor_values(data, factor)
-        Enum.reduce child_nodes_values, tree, fn x, acc ->
-          Map.put(acc, x, generate_tree(%{}, filter_data(data, factor, x)))
+
+        Enum.reduce child_nodes_values, tree, fn value, acc ->
+          Map.put(acc, value, generate_tree(filter_data(data, factor, value)))
         end
     end
   end
 
-  def is_yes_or_no(data) do
+  # Checks whether rest of data map contains only positive or negative examples
+  @spec is_yes_or_no(%{}) :: :yes | :no | :look_further
+  defp is_yes_or_no(data) do
     cond do
       length(data) == filter_data(data, "decision", 1) |> length -> :yes
       length(data) == filter_data(data, "decision", 0) |> length -> :no
@@ -70,30 +53,33 @@ defmodule ID3 do
     end
   end
 
-  def get_factor_values(data, factor) do
+  @spec get_factor_values(%{}, String.t()) :: [String.t()]
+  defp get_factor_values(data, factor) do
     factor_index = get_factor_index(factor)
     data
     |> Enum.map(fn x -> Enum.at(x, factor_index) end)
     |> Enum.uniq
   end
 
-  def filter_data(data, factor, value) do
+  @spec filter_data(%{}, String.t(), number | String.t()) :: %{}
+  defp filter_data(data, factor, value) do
     index = get_factor_index(factor)
     Enum.filter(data, fn x -> Enum.at(x, index) == value end)
   end
 
-  def get_highest_gain([], _data, {factor, _gain}), do: factor
-
-  def get_highest_gain([head | tail], data, {_factor, gain} = highest_gain) do
+  @spec get_factor_with_highest_gain([String.t()], %{}, {String.t(), number}) :: String.t()
+  defp get_factor_with_highest_gain([], _data, {factor, _gain}), do: factor
+  defp get_factor_with_highest_gain([head | tail], data, {_factor, gain} = highest_gain) do
     current_gain = gain(data, head)
     if current_gain > gain do
-      get_highest_gain(tail, data, {head, current_gain})
+      get_factor_with_highest_gain(tail, data, {head, current_gain})
     else
-      get_highest_gain(tail, data, highest_gain)
+      get_factor_with_highest_gain(tail, data, highest_gain)
     end
   end
 
-  def gain(data, factor) do
+  @spec gain(%{}, String.t()) :: number
+  defp gain(data, factor) do
     decision_entrophy = decision_entrophy(data)
     possible_values = get_factor_values(data, factor)
 
@@ -111,7 +97,7 @@ defmodule ID3 do
       |> Float.round(3)
   end
 
-  def decision_entrophy(data) do
+  defp decision_entrophy(data) do
     total_amount = length(data)
     yes_amount = decision_amount(data, 1)
     no_amount = decision_amount(data, 0)
@@ -119,13 +105,13 @@ defmodule ID3 do
     entrophy(yes_amount, no_amount, total_amount)
   end
 
-  def decision_amount(list, expected_value) do
+  defp decision_amount(list, expected_value) do
     list
     |> Enum.filter(fn x -> List.last(x) == expected_value end)
     |> length
   end
 
-  def factors_entrophy(data, factor_name, factor_value) do
+  defp factors_entrophy(data, factor_name, factor_value) do
     total_amount = factor_amount(data, factor_name, factor_value)
     yes_amount = factor_amount(data, factor_name, factor_value, 1)
     no_amount = factor_amount(data, factor_name, factor_value, 0)
@@ -133,23 +119,23 @@ defmodule ID3 do
     entrophy(yes_amount, no_amount, total_amount)
   end
 
-  def entrophy(yes, no, total) do
+  defp entrophy(yes, no, total) do
     p_yes = probability(yes, total)
     p_no = probability(no, total)
     (-(p_yes * log2(p_yes)) - (p_no * log2(p_no))) |> Float.round(3)
   end
 
-  def log2(0.0), do: 0
+  defp log2(0.0), do: 0
 
-  def log2(value) do
+  defp log2(value) do
     :math.log2(value)
   end
 
-  def probability(cases, total) do
+  defp probability(cases, total) do
     cases / total
   end
 
-  def factor_amount(data, factor_name, expected_value) do
+  defp factor_amount(data, factor_name, expected_value) do
     factor_index = get_factor_index(factor_name)
 
     data
@@ -157,7 +143,7 @@ defmodule ID3 do
     |> length
   end
 
-  def factor_amount(data, factor_name, expected_value, decision) do
+  defp factor_amount(data, factor_name, expected_value, decision) do
     factor_index = get_factor_index(factor_name)
 
     data
@@ -166,18 +152,20 @@ defmodule ID3 do
     |> length
   end
 
-  def is_factor_clean(data, factor, value) do
-    factor_amount(data, factor, value, 0) == 0
-  end
-
-  def get_factor_index(factor_name) do
+  @spec get_factor_index(String.t()) :: Integer
+  defp get_factor_index(factor_name) do
     factor_name
       |> String.to_atom()
       |> (&Map.get(@data_indexes, &1)).()
   end
-end
-IO.puts "DFVDFVDFV"
-{:ok, file} = File.open "tree.json", [:write]
-json = ID3.generate_tree(%{}, ID3.data()) |> Poison.encode!()
-IO.binwrite file, json
 
+  def main() do
+    {:ok, file} = File.open "tree.json", [:write]
+    json = ID3.generate_tree(ID3.data()) |> Poison.encode!()
+    IO.binwrite file, json
+  end
+end
+# IO.puts "DFVDFVDFV"
+# {:ok, file} = File.open "tree.json", [:write]
+# json = ID3.generate_tree(ID3.data()) |> Poison.encode!()
+# IO.binwrite file, json
